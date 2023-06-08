c843ac2e681d7cbb82cbe6e23c4e6f728f8f5e62
Edouard Zemb <edouard.zemb@gmail.com>
2023-06-08 22:01:04 +0200

    test: Add JUnit 5 tests for UserInputHandler class
    This commit adds JUnit 5 unit tests for the UserInputHandler class. The tests cover the behavior of the handleUserInput() method when the 'exit' command is entered. The OutputProvider and InputProvider mocks are used to simulate user input and validate the expected interactions. The tests provide coverage and ensure the correct functionality of the UserInputHandler class.
    
    Refs #1

---
+++ Learning Progress Tracker (Java)/task/test/tracker/UserInputHandlerTests.java
@@ -0,0 +1,41 @@
+package tracker;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+
+@DisplayName("UserInputHandler Tests")
+public class UserInputHandlerTests {
+    @Mock
+    private OutputProvider outputProvider;
+    @Mock
+    private InputProvider inputProvider;
+    private AutoCloseable closeable;
+    private UserInputHandler userInputHandler;
+
+    @BeforeEach
+    void setup() {
+        closeable = MockitoAnnotations.openMocks(this);
+        userInputHandler = new UserInputHandler(inputProvider, outputProvider);
+    }
+
+    @AfterEach
+    void close() throws Exception {
+        closeable.close();
+    }
+    @Test
+    @DisplayName("handleUserInput() exits when 'exit' command is entered")
+    void testHandleUserInputExitsWhenExitCommandEntered() {
+        when(inputProvider.getInput()).thenReturn("command1", "command2", "exit");
+
+        userInputHandler.handleUserInput();
+
+        verify(outputProvider).print("Bye!");
+        verify(inputProvider, times(3)).getInput();
+    }
+}
