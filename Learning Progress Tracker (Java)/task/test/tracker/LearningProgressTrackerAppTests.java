78ec775e208de829fc367772b1bc2590b524704c
Edouard Zemb <edouard.zemb@gmail.com>
2023-06-08 12:15:23 +0200

    refactor: Refactor LearningProgressTrackerAppTests
    Refactor the `learningProgressTrackerAppRun` test in the `LearningProgressTrackerAppTests` class to use a mock `OutputProvider`. This ensures test isolation and removes the dependency on concrete implementations. The test now verifies that the `run()` method of `LearningProgressTrackerApp` correctly prints the expected output message "Learning Progress Tracker".
    
    Refs #1

--- Learning Progress Tracker (Java)/task/test/tracker/LearningProgressTrackerAppTests.java
+++ Learning Progress Tracker (Java)/task/test/tracker/LearningProgressTrackerAppTests.java
@@ -1,15 +1,34 @@
 package tracker;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @DisplayName("LearningProgressTrackerApp Tests")
 public class LearningProgressTrackerAppTests {
+    @Mock
+    private OutputProvider outputProvider;
+    @Mock
+    private InputProvider inputProvider;
+    private AutoCloseable closeable;
+
+    private LearningProgressTrackerApp app;
+
+    @BeforeEach
+    void setup() {
+        closeable = MockitoAnnotations.openMocks(this);
+        app = new LearningProgressTrackerApp(outputProvider, inputProvider);
+    }
+
+    @AfterEach
+    void close() throws Exception {
+        closeable.close();
+    }
 
     @Test
     @DisplayName("LearningProgressTrackerApp run() should print the correct message")
@@ -32,5 +51,15 @@ public class LearningProgressTrackerAppTests {
         String expectedOutput = "Learning Progress Tracker";
         String actualOutput = outputStream.toString().trim();
         assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    @DisplayName("LearningProgressTrackerApp run() should print app title")
+    public void learningProgressTrackerAppRunShouldPrintAppTitle() {
+        when(inputProvider.getInput()).thenReturn("exit");
+
+        app.run();
+
+        verify(outputProvider).print("Learning Progress Tracker");
     }
 }
