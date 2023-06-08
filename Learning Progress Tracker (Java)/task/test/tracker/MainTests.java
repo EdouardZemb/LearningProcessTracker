5f89d75f6c715454e6a03910ae8012e85c8de44a
Edouard Zemb <edouard.zemb@gmail.com>
2023-06-08 12:37:51 +0200

    chore: Normalize line separators in MainTests
    Refactor the MainTests class to normalize line separators in the expected and actual outputs. This allows for consistent comparison of the outputs, ignoring differences in line separators. The `normalizeLineSeparators` method is added to handle this normalization process.
    
    Refs #1

--- Learning Progress Tracker (Java)/task/test/tracker/MainTests.java
+++ Learning Progress Tracker (Java)/task/test/tracker/MainTests.java
@@ -3,13 +3,18 @@ package tracker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Main Tests")
 public class MainTests {
+
+    private static final Pattern LINE_SEPARATOR_PATTERN = Pattern.compile("\\R");
 
     @Test
     @DisplayName("Main main() should initialize and run LearningProgressTrackerApp")
@@ -18,17 +23,30 @@ public class MainTests {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
 
+        // Set the input to simulate user interaction
+        String input = "exit";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
         // Call the main() method
         Main.main(null);
 
-        // Restore the standard output
+        // Restore the standard output and input
         System.setOut(System.out);
+        System.setIn(System.in);
 
         // Verify the output
-        String expectedOutput = "Learning Progress Tracker";
-        String actualOutput = outputStream.toString().trim();
+        String expectedOutput = "Learning Progress Tracker\nBye!";
+
+        // Normalize line separators
+        expectedOutput = normalizeLineSeparators(expectedOutput);
+        String actualOutput = normalizeLineSeparators(outputStream.toString().trim());
         assertEquals(expectedOutput, actualOutput);
     }
 
     // Additional test cases for Main class can be added here
+
+    private static String normalizeLineSeparators(String text) {
+        return LINE_SEPARATOR_PATTERN.matcher(text).replaceAll("\n");
+    }
 }
