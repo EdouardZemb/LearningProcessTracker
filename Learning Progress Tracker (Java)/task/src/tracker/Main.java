2c7b6d78e1ba88da2b8a892c9552fc3bccb14667
Edouard Zemb <edouard.zemb@gmail.com>
2023-06-10 09:07:26 +0200

    refactor: Refactor Main class for improved dependency injection
    Extracted creation of dependencies into separate methods for better modularity and testability.
    Introduced a registerCommands() method to handle command registration, separating concerns.
    Modified run() method to call registerCommands() before invoking the run() method of the application.
    Updated the Main class constructor to accept necessary dependencies (outputProvider, commandRegistry, and app) for improved loose coupling.
    Applied dependency injection principles to enhance maintainability, testability, and flexibility.
    
    Refs #7

--- Learning Progress Tracker (Java)/task/src/tracker/Main.java
+++ Learning Progress Tracker (Java)/task/src/tracker/Main.java
@@ -1,18 +1,52 @@
 package tracker;
 
 public class Main {
-    public static void main(String[] args) {
-        OutputProvider outputProvider = new ConsoleOutputProvider();
-        InputProvider inputProvider = new SystemInputProvider();
-        CommandRegistry defaultCommandRegistry = new CommandRegistry();
+    private final OutputProvider outputProvider;
+    private final CommandRegistry commandRegistry;
+    private final LearningProgressTrackerApp app;
+
+    public Main(OutputProvider outputProvider,
+                CommandRegistry commandRegistry,
+                LearningProgressTrackerApp app) {
+        this.outputProvider = outputProvider;
+        this.commandRegistry = commandRegistry;
+        this.app = app;
+    }
+
+    private void registerCommands() {
         Command addStudentCommand = new AddStudentCommand(outputProvider);
         Command exitCommand = new ExitCommand(outputProvider);
-        addStudentCommand.register(defaultCommandRegistry);
-        exitCommand.register(defaultCommandRegistry);
-        CommandExecutor commandExecutor = new DefaultCommandExecutor(outputProvider);
-        UserInputHandler userInputHandler = new UserInputHandler(inputProvider, defaultCommandRegistry, commandExecutor);
-        LearningProgressTrackerApp app = new LearningProgressTrackerApp(outputProvider, userInputHandler);
+        addStudentCommand.register(commandRegistry);
+        exitCommand.register(commandRegistry);
+    }
 
+    public void run() {
+        registerCommands();
         app.run();
     }
+
+    public static void main(String[] args) {
+        OutputProvider outputProvider = createOutputProvider();
+        InputProvider inputProvider = createInputProvider();
+        CommandRegistry commandRegistry = createCommandRegistry();
+        CommandExecutor commandExecutor = new DefaultCommandExecutor(outputProvider);
+        UserInputHandler userInputHandler = new UserInputHandler(inputProvider, commandRegistry, commandExecutor);
+        LearningProgressTrackerApp app = new LearningProgressTrackerApp(outputProvider, userInputHandler);
+
+        Main main = new Main(outputProvider, commandRegistry, app);
+        main.run();
+    }
+
+    private static OutputProvider createOutputProvider() {
+        return new ConsoleOutputProvider();
+    }
+
+    private static InputProvider createInputProvider() {
+        return new SystemInputProvider();
+    }
+
+    private static CommandRegistry createCommandRegistry() {
+        return new CommandRegistry();
+    }
 }
+
